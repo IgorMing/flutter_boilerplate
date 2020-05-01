cc37075699f2e656e9e650b91daa4e797dcce51e
Igor Mesquita <igormesquita@MacBook-Pro.local>
2020-05-01 18:53:39 -0300

    make text input slightly smaller (height)

--- lib/widgets/text_input.dart
+++ lib/widgets/text_input.dart
@@ -8,9 +8,11 @@ class TextInput extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      // height: 42.0,
       child: TextField(
         decoration: InputDecoration(
           hintText: this.hintText,
+          contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 12),
           border: OutlineInputBorder(
             borderRadius: const BorderRadius.all(
               const Radius.circular(2.0),
